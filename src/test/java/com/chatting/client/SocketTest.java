53d906f095fb56cf01326676c366a292e72862cf
JeongJiSang <jsj00203@gmail.com>
2023-03-13 17:17:38 +0900

    클라이언트 예제

--- src/test/java/com/chatting/client/SocketTest.java
+++ src/test/java/com/chatting/client/SocketTest.java
@@ -1,11 +1,14 @@
 package com.chatting.client;
 
+import com.chatting.client.model.Client1;
 import org.junit.Test;
+
+import java.net.Socket;
 
 public class SocketTest {
 
     @Test
     void socketTest1() {
-
+        Client1 client1 = new Client1("100.100.100.56", 9100);
     }
 }
