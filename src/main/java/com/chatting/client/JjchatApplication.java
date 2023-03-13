72d090350d598e9446284e5c30da0703b44d47f3
JeongJiSang <jsj00203@gmail.com>
2023-03-13 15:17:12 +0900

    initialize client socket sample

--- src/main/java/com/chatting/client/JjchatApplication.java
+++ src/main/java/com/chatting/client/JjchatApplication.java
@@ -1,9 +1,13 @@
 package com.chatting.client;
 
+import com.chatting.client.model.Client1;
+
 public class JjchatApplication {
 
 	public static void main(String[] args) {
-
+		String ip = "연결할 주소(ip)";
+		int port = 9100;
+		new Client1(ip, port);
 	}
 
 }
